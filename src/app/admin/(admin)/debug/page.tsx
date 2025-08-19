5967a417b8c63090e5a74c26efb0e86e4cf27403
fadhly-git <fakeherex160@gmail.com>
2025-08-19 10:43:21 +0700

    feat: add debug page

---
+++ src/app/admin/(admin)/debug/page.tsx
@@ -0,0 +1,12 @@
+import { DebugClient } from '@/components/debug/debug-client';
+
+const DebugPage = () => {
+    return (
+        <div>
+            <h1>Debug Page</h1>
+            <DebugClient />
+        </div>
+    );
+};
+
+export default DebugPage;
